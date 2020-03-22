b688574078088a30b885c1b50f8bd97d9b9d46ea
Matt Turner <matturner@gmail.com>
2020-03-22 21:03:07 +0000

    Added a couple of endpoints

--- README.md
+++ README.md
@@ -5,8 +5,5 @@ Useful for testing blue/green deployment mechanisms, or, deploying onto
 machines that you otherwise couldn't tell apart.
 
 ## TODO
-* /api/v1 endpoint that returns type:application/json { colour: blue }
-* /healthz endpoint that returns 200
-* /live endpoint that returns 200 after 5s
 * :blank tag that has nothing in the environment so users can easily set
   their own (and because k8s will do a rolling update for an env change)
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 /* Tested at 1000rps: p50 3ms, p99 33ms, 40% of a 2GHz AMD Turion(tm) II Neo N54L */
 
+extern crate chrono;
 extern crate tiny_http;
 
 use std::env;
 
 fn main() {
+    let start = chrono::Utc::now();
     let colour = env::var("COLOUR").expect("Please supply a colour");
-    let body = format!("<html><head><title>{c}</title></head><body style=\"background-color: {c}; color: white\"><h1>{c}</h1><body><html>", c=colour);
+    let html = format!("<html><head><title>{c}</title></head><body style=\"background-color: {c}; color: white\"><h1>{c}</h1><body><html>", c=colour);
+    let json = format!("{{ \"colour\": \"{c}\" }}", c = colour); // Not taking on the heft of serde for this
 
     let server = tiny_http::Server::http("0.0.0.0:8080").expect("Can't start server");
     println!("Listening on {:?}", server.server_addr());
 
     for request in server.incoming_requests() {
-        let response = tiny_http::Response::from_string(body.clone()).with_header(
-            "Content-Type: text/html"
-                .parse::<tiny_http::Header>()
-                .unwrap(),
-        );
-        let _ = request.respond(response);
+        if request.url().starts_with("/live") && (chrono::Utc::now() - start).num_seconds() < 5 {
+            let response = tiny_http::Response::new_empty(tiny_http::StatusCode(500));
+            let _ = request.respond(response);
+        } else {
+            let (r, t) = if request.url().starts_with("/api") {
+                (json.clone(), "Content-Type: application/json")
+            } else {
+                (html.clone(), "Content-Type: text/html")
+                /* this will also reply 200 to [/healthz,/live], so that's "fine" */
+            };
+            let response = tiny_http::Response::from_string(r)
+                .with_header(t.parse::<tiny_http::Header>().unwrap());
+            let _ = request.respond(response);
+        }
     }
 }
